8baed7dad80d95125396ac699f8d76d229b9f807
Pijush Manna <pijushmanna12344@gmail.com>
2021-08-27 01:20:49 +0530

    Step 2

--- app/src/main/java/com/blogspot/desymediaplayer/PlayerActivity.kt
+++ app/src/main/java/com/blogspot/desymediaplayer/PlayerActivity.kt
@@ -2,12 +2,17 @@ package com.blogspot.desymediaplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.blogspot.desymediaplayer.databinding.ActivityPlayerBinding
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.util.Util
 
+private const val TAG = "ActivityPlayer"
 class PlayerActivity : AppCompatActivity() {
     private val viewBinding by lazy{
         ActivityPlayerBinding.inflate(layoutInflater)
@@ -16,6 +21,7 @@ class PlayerActivity : AppCompatActivity() {
     private var playerWindowIndex = 0
     private var currentPlaybackPosition = 0L
     private var playWhenReady = true
+    private val playBackState = playbackStateListener()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,21 +44,22 @@ class PlayerActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-        if (Util.SDK_INT >= 24){
+        if (player!=null){
             releasePlayer()
         }
     }
 
     override fun onPause() {
         super.onPause()
-        if (Util.SDK_INT < 24){
-            releasePlayer()
-        }
+        releasePlayer()
     }
 
     private fun initializePlayer(){
         val trackSelector = DefaultTrackSelector(this).apply {
-            setParameters(buildUponParameters().setMaxVideoSizeSd())
+            parameters = buildUponParameters()
+                .setMaxVideoSizeSd()
+                .setPreferredAudioLanguage("dubbed")
+                .build()
         }
         player = SimpleExoPlayer.Builder(this)
             .setTrackSelector(trackSelector)
@@ -64,13 +71,31 @@ class PlayerActivity : AppCompatActivity() {
                 exoPlayer.playWhenReady = playWhenReady
                 exoPlayer.seekTo(playerWindowIndex,currentPlaybackPosition)
                 exoPlayer.prepare()
+                exoPlayer.addListener(playBackState)
             }
     }
 
     private fun releasePlayer(){
         player?.run {
+            playerWindowIndex = this.currentWindowIndex
+            currentPlaybackPosition = this.currentPosition
+            this@PlayerActivity.playWhenReady = this.playWhenReady
             release()
         }
         player = null
     }
+
+
+    private fun playbackStateListener() = object : Player.EventListener {
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            when (playbackState) {
+                ExoPlayer.STATE_IDLE ->  viewBinding.pbrBuffering.visibility = View.INVISIBLE
+                ExoPlayer.STATE_BUFFERING -> viewBinding.pbrBuffering.visibility = View.VISIBLE
+                ExoPlayer.STATE_READY -> viewBinding.pbrBuffering.visibility = View.INVISIBLE
+                ExoPlayer.STATE_ENDED ->  viewBinding.pbrBuffering.visibility = View.INVISIBLE
+                else ->  viewBinding.pbrBuffering.visibility = View.INVISIBLE
+            }
+        }
+    }
 }
+
