a2f4c3f94178e7b1eb4e7159d55c9ef9ad58b946
Pijush Manna <pijushmanna12344@gmail.com>
2021-08-26 23:20:40 +0530

    Step 1

--- app/src/main/java/com/blogspot/desymediaplayer/PlayerActivity.kt
+++ app/src/main/java/com/blogspot/desymediaplayer/PlayerActivity.kt
@@ -2,10 +2,75 @@ package com.blogspot.desymediaplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blogspot.desymediaplayer.databinding.ActivityPlayerBinding
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.util.Util
 
 class PlayerActivity : AppCompatActivity() {
+    private val viewBinding by lazy{
+        ActivityPlayerBinding.inflate(layoutInflater)
+    }
+    private var player: SimpleExoPlayer? = null
+    private var playerWindowIndex = 0
+    private var currentPlaybackPosition = 0L
+    private var playWhenReady = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        setContentView(viewBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24){
+            initializePlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT < 24 || player == null){
+            initializePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24){
+            releasePlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24){
+            releasePlayer()
+        }
+    }
+
+    private fun initializePlayer(){
+        val trackSelector = DefaultTrackSelector(this).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())
+        }
+        player = SimpleExoPlayer.Builder(this)
+            .setTrackSelector(trackSelector)
+            .build()
+            .also { exoPlayer ->
+                viewBinding.videoView.player = exoPlayer
+                val mediaItem = MediaItem.fromUri("https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8")
+                exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(playerWindowIndex,currentPlaybackPosition)
+                exoPlayer.prepare()
+            }
+    }
+
+    private fun releasePlayer(){
+        player?.run {
+            release()
+        }
+        player = null
     }
 }
